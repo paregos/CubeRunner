d1620957ccd508b6f37a2812be144397bb679aba
paregos <paregos18@gmail.com>
2019-07-18 22:51:06 +1000

    Made basic level generation

--- CubeRunner/Assets/Scripts/LevelGenerator.cs
+++ CubeRunner/Assets/Scripts/LevelGenerator.cs
@@ -1,32 +1,98 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Assets.Scripts;
 using UnityEngine;
 
-public class LevelGenerator : MonoBehaviour {
+public class LevelGenerator : MonoBehaviour
+{
 
-    public GameObject baseCube;
+
+    private float _blockWidth = (float)Math.Pow(2f, 0.5f);
+    private float _halfblockWidth = ((float)Math.Pow(2f, 0.5f))/2f;
+
+    public int initialRowCount = 10;
+    public int initialRowWidth = 7;
+
+    public GameObject wallBlock;
 
     // Use this for initialization
     void Start () {
-        for (int i = 1; i < 100; i++)
+        for (int i = 0; i < initialRowCount; i++)
         {
-            Vector3 leftCubeFinalPosition = new Vector3(i - 3f, 0.5f, i + 3f);
-            Vector3 rightCubeFinalPosition = new Vector3(i + 3f, 0.5f, i - 3f);
-
-            BaseCubeController leftCube = Instantiate(
-                    baseCube, 
-                    new Vector3(leftCubeFinalPosition.x, leftCubeFinalPosition.y-10, leftCubeFinalPosition.z),
-                    Quaternion.identity)
-                .GetComponent<BaseCubeController>();
-            BaseCubeController rightCube = Instantiate(
-                baseCube,
-                new Vector3(rightCubeFinalPosition.x, rightCubeFinalPosition.y - 10, rightCubeFinalPosition.z),
-                Quaternion.identity)
-                .GetComponent<BaseCubeController>();
-
-            leftCube.MoveToPosition(leftCubeFinalPosition);
-            rightCube.MoveToPosition(rightCubeFinalPosition);
+            SpawnNextRow(i);
         }
 	}
+
+    public void SpawnNextRow(float rowNumber)
+    {
+        var isSmallRow = rowNumber % 2 == 1;
+        var floorRowWidth = isSmallRow ? initialRowWidth - 1 : initialRowWidth;
+
+        SpawnWalls(rowNumber, isSmallRow);
+
+        //Floor blocks
+        //TODO - make this flexible
+        for (int i = 0; i < floorRowWidth; i++)
+        {
+            if (isSmallRow)
+            {
+                FloorBlockController floorBlock = Instantiate(wallBlock, 
+                        new Vector3(
+                            rowNumber * _blockWidth / 2f,
+                            0.1f,
+                            _blockWidth + _halfblockWidth + i * _blockWidth),
+                        Quaternion.Euler(0, 45, 0))
+                    .GetComponent<FloorBlockController>();
+            }
+            else
+            {
+                FloorBlockController floorBlock = Instantiate(wallBlock, 
+                        new Vector3(
+                            rowNumber * _halfblockWidth,
+                            0.1f,
+                            _blockWidth + i * _blockWidth),
+                        Quaternion.Euler(0, 45, 0))
+                    .GetComponent<FloorBlockController>();
+            }
+        }
+    }
+
+    private void SpawnWalls(float rowNumber, bool isSmallRow)
+    {
+        Vector3 leftBlockFinalPosition;
+        Vector3 rightBlockFinalPosition;
+
+        if (isSmallRow)
+        {
+            leftBlockFinalPosition = new Vector3(
+                rowNumber * _blockWidth / 2f,
+                0.5f,
+                _halfblockWidth);
+            rightBlockFinalPosition = new Vector3(
+                rowNumber * _blockWidth / 2f,
+                0.5f,
+                initialRowWidth * _blockWidth + _halfblockWidth);
+        }
+        else
+        {
+            leftBlockFinalPosition = new Vector3(
+                rowNumber * _halfblockWidth,
+                0.5f,
+                0);
+            rightBlockFinalPosition = new Vector3(
+                rowNumber * _halfblockWidth,
+                0.5f,
+                _blockWidth + initialRowWidth * _blockWidth);
+        }
+
+        WallBlockController leftWallBlock = Instantiate(wallBlock, new Vector3(leftBlockFinalPosition.x, leftBlockFinalPosition.y - 10, leftBlockFinalPosition.z), Quaternion.Euler(0, 45, 0))
+            .GetComponent<WallBlockController>();
+        WallBlockController rightWallBlock = Instantiate(wallBlock, new Vector3(rightBlockFinalPosition.x, rightBlockFinalPosition.y - 10, rightBlockFinalPosition.z), Quaternion.Euler(0, 45, 0))
+            .GetComponent<WallBlockController>();
+
+        leftWallBlock.MoveToPosition(leftBlockFinalPosition);
+        rightWallBlock.MoveToPosition(rightBlockFinalPosition);
+    }
 
 }
--- CubeRunner/Assets/Scripts/WallBlockController.cs
+++ CubeRunner/Assets/Scripts/WallBlockController.cs
@@ -1,15 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Assets.Scripts;
 using DG.Tweening;
 using UnityEngine;
 
-public class BaseCubeController : MonoBehaviour
+public class WallBlockController : MonoBehaviour, IBlockController
 {
     public Material baseMaterial;
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
@@ -19,8 +19,8 @@ public class BaseCubeController : MonoBehaviour
 
     public void MoveToPosition(Vector3 position)
     {
-        transform.DOMove(position, 5);
-        transform.DOShakeRotation(10);
-        baseMaterial.DOFade(255, 10);
+        transform.DOMove(position, 2);
+        transform.DOShakeRotation(2);
+        baseMaterial.DOFade(255, 3);
     }
 }
